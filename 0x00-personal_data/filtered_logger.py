9ac9fe1352a490c9945502f57e6f655498d540fd
EuniceGeorge <georgeunice408.com>
2024-11-09 02:09:43 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ filter datum """
 
+import logging
 from typing import List
 import re
 
@@ -11,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str,
 
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+if __name__ == '__main__':
+    main()
