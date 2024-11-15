5f05deddb7f5199bf193ee2931e4c871908d8bdb
EuniceGeorge <georgeunice408.com>
2024-11-15 11:43:34 +0100

    push

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""manage API"""
+from flask import request
+
+
+class Auth(object):
+    """manage API"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method that returns boolean"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorizedmethod that returns boolean"""
+        if request is None:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user method"""
+        if request is None:
+            return None
