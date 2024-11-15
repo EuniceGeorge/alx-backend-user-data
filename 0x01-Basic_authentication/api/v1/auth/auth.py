fccca6153408df308d7abc92674416cfe89165df
EuniceGeorge <georgeunice408.com>
2024-11-15 13:06:57 +0100

    push

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,12 @@ from typing import List
 class Auth(object):
     """manage API"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """method that returns boolean"""
+        """method that returns boolean
+        Args:
+            path (str): The path to check.
+            excluded_paths (List[str]): A list of paths
+        Returns:
+            bool: Currently always returns False."""
         return False
 
     def authorization_header(self, request=None) -> str:
