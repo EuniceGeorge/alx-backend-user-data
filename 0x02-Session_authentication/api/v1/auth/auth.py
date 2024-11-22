64c257a23bfb08a8178ecba031c44e8422db37b2
EuniceGeorge <georgeunice408.com>
2024-11-22 04:09:52 +0100

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""manage API Authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth(object):
+    """manage API"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method that returns boolean"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        """check if path is excluded"""
+        for path in excluded_paths:
+            if path.endswith('*') and path.startswith(path[:-1]):
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorizedmethod that returns boolean"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user method"""
+        return None
+
+
+class BasicAuth(Auth):
+    """inheritance"""
